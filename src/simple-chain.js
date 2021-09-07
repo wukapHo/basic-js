68dc501a8de6e46fe3dd433b92787dd8bedab64f
Vyacheslav Likashkov <xb0ksx@gmail.com>
2021-09-07 13:42:35 +0300

    feat: complete simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,38 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (arguments.length === 0) {
+      this.chain.push('( )');
+      return this;
+    } else {
+      this.chain.push(`( ${value} )`);
+      return this;
+    }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position >= this.chain.length || position <= 0 || isNaN(position)) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = this.chain.join('~~');
+    this.chain = [];
+    return str;
   }
 };
